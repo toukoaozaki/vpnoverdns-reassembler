a3e4b583bd2534996f4e0890ec9b7933bf9cb40d
Eunsoo Roh <roh7@illinois.edu>
2015-03-10 15:56:40 -0500

    Fixed indentation.

--- vodreassembler/util.py
+++ vodreassembler/util.py
@@ -3,12 +3,12 @@
 import struct
 
 def ipv4_to_bytes(addr):
-    octets = addr.split('.')
-    if len(octets) != 4:
-        return False
-    octets = list(map(int, octets))
-    # Every octet must fall in range [0,255]
-    for o in octets:
-        if not 0 <= o <= 255:
-            return False
-    return b''.join(map(lambda x: struct.pack('!B', x), octets))
+  octets = addr.split('.')
+  if len(octets) != 4:
+    return False
+  octets = list(map(int, octets))
+  # Every octet must fall in range [0,255]
+  for o in octets:
+    if not 0 <= o <= 255:
+      return False
+  return b''.join(map(lambda x: struct.pack('!B', x), octets))
