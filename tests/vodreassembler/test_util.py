349a85dbb0fb4065277b66f627f3de25322ecbfa
Eunsoo Roh <roh7@illinois.edu>
2015-04-27 20:00:11 -0500

    Added runtests.py and utility test stub.

---
+++ tests/vodreassembler/test_util.py
@@ -0,0 +1,9 @@
+import unittest
+import vodreassembler
+
+class TestUtil(unittest.TestCase):
+  def test_placeholder(self):
+    self.assertTrue(True)
+
+if __name__ == '__main__':
+  unittest.main()
