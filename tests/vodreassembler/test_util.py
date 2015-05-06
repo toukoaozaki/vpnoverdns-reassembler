5a19befd2769c4be62d4c1a64eef9f528d56d8fe
Eunsoo Roh <roh7@illinois.edu>
2015-05-05 21:56:34 -0500

    Added DataAssembler class and tests.

--- tests/vodreassembler/test_util.py
+++ tests/vodreassembler/test_util.py
@@ -1,5 +1,205 @@
+import itertools
+import os
 import unittest
 from vodreassembler import util
+
+
+class TestDataAssembler(unittest.TestCase):
+  def test_init_bad_args(self):
+    with self.assertRaises(TypeError):
+      util.DataAssembler(None)  # alignment must be int
+    with self.assertRaises(TypeError):
+      util.DataAssembler(1.1)  # alignment must be int
+    with self.assertRaises(ValueError):
+      util.DataAssembler(-1)  # alignment must be positive
+    with self.assertRaises(ValueError):
+      util.DataAssembler(0)  # alignment must be positive
+    with self.assertRaises(TypeError):
+      util.DataAssembler(1, length=1.1)  # size must be int or None
+    with self.assertRaises(TypeError):
+      util.DataAssembler(1, length='asdf')  # size must be int or None
+    with self.assertRaises(ValueError):
+      util.DataAssembler(1, length=-1)  # size must be non-negative
+
+  def test_getbytes_incomplete(self):
+    assembler = util.DataAssembler(2, length=5)
+    with self.assertRaises(util.IncompleteDataError):
+      assembler.getbytes()
+    self.assertEquals(b'\x00\x00\x00\x00\x00',
+                      assembler.getbytes(incomplete=True))
+    assembler.add(b'\x01\x02', 2)
+    with self.assertRaises(util.IncompleteDataError):
+      assembler.getbytes()
+    self.assertEquals(b'\x00\x00\x01\x02\x00',
+                      assembler.getbytes(incomplete=True))
+
+  def test_length_deduction(self):
+    assembler = util.DataAssembler(3)
+    self.assertIsNone(assembler.length)
+    assembler.add(b'\xff\xfe\xfd', 0)
+    self.assertIsNone(assembler.length)
+    assembler.add(b'\x01\x02', 6)
+    self.assertEquals(8, assembler.length)
+    assembler.add(b'\xfc\xfb\x00', 3)
+    self.assertEquals(b'\xff\xfe\xfd\xfc\xfb\x00\x01\x02', assembler.getbytes())
+
+  def test_incorrect_sized_chunks(self):
+    assembler = util.DataAssembler(3)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00' * (assembler.alignment + 1), 0)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00' * (assembler.alignment - 1), 0)
+    assembler.add(b'\xff\xfe\xfd', 3)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00' * (assembler.alignment + 1), 0)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00' * (assembler.alignment - 1), 0)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00' * (assembler.alignment + 1), 6)
+    self.assertEquals(b'\x00\x00\x00\xff\xfe\xfd',
+                      assembler.getbytes(incomplete=True))
+    # Test with predefined length
+    assembler = util.DataAssembler(3, length=5)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00\x00\x00\x00', 0)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00\x00\x00\x00', 3)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00', 3)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00\x00\x00', 3)
+    assembler.add(b'\xba\xbe', 3)
+    self.assertEquals(b'\x00\x00\x00\xba\xbe',
+                      assembler.getbytes(incomplete=True))
+    assembler = util.DataAssembler(3, length=6)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00', 3)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00\x00', 3)
+    assembler.add(b'\xfe\xba\xbe', 3)
+    self.assertEquals(b'\x00\x00\x00\xfe\xba\xbe',
+                      assembler.getbytes(incomplete=True))
+
+  def test_collisions(self):
+    assembler = util.DataAssembler(3)
+    assembler.add(b'\x00\x01\x00', 3)
+    with self.assertRaises(util.ChunkCollisionError):
+      assembler.add(b'\x00\x02\x00', 3)
+    assembler.add(b'\x00\x03\x00', 0)
+    with self.assertRaises(util.ChunkCollisionError):
+      assembler.add(b'\x00\x04\x00', 0)
+    assembler.add(b'\x00\x05', 6)
+    with self.assertRaises(util.ChunkCollisionError):
+      assembler.add(b'\x00\x06', 6)
+    with self.assertRaises(util.ChunkCollisionError):
+      assembler.add(b'\x00', 6)
+    # Collision with too large chunk should be handled as ValueError.
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00\x00\x00\x00', 6)
+    self.assertEquals(b'\x00\x03\x00\x00\x01\x00\x00\x05',
+                      assembler.getbytes())
+
+  def test_maligned_offsets(self):
+    assembler = util.DataAssembler(3)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00\x00\x00', 1)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00\x00\x00', 2)
+    assembler.add(b'\xca\xfe\xba', 0)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00\x00\x00', 4)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00\x00\x00', 5)
+    assembler.add(b'\xbe\x00\xff', 3)
+    self.assertEquals(b'\xca\xfe\xba\xbe\x00\xff',
+                      assembler.getbytes())
+
+  def test_out_of_bound_offsets(self):
+    assembler = util.DataAssembler(3)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00\x00\x00', -1)
+    assembler = util.DataAssembler(3, length=3)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00\x00\x00', -1)
+    with self.assertRaises(util.ChunkPastEndError):
+      assembler.add(b'\x00\x00\x00', 3)
+    assembler = util.DataAssembler(3, length=4)
+    with self.assertRaises(ValueError):
+      assembler.add(b'\x00\x00\x00', -1)
+    with self.assertRaises(util.ChunkPastEndError):
+      assembler.add(b'\x00\x00\x00', 6)
+
+  def _generate_data(self, length, alignment):
+    assert length > 0
+    assert alignment > 0
+    data = os.urandom(length)
+    return data, [util.DataChunk(data[i:i+alignment], i)
+                  for i in range(0, len(data), alignment)]
+
+  def _test_sized_add(self, length, alignment):
+    data, chunks = self._generate_data(length, alignment)
+    # Test all possible permutations
+    for sequence in itertools.permutations(chunks):
+      assembler = util.DataAssembler(alignment, length=length)
+      for chunk in sequence:
+        assembler.add(chunk.data, chunk.offset)
+      self.assertEquals(data, assembler.getbytes())
+      # Same test with add_chunk()
+      assembler = util.DataAssembler(alignment, length=length)
+      for chunk in sequence:
+        assembler.add_chunk(chunk)
+      self.assertEquals(data, assembler.getbytes())
+
+  def test_sized_add_3_1(self):
+    self._test_sized_add(3, 1)
+
+  def test_sized_add_9_2(self):
+    self._test_sized_add(9, 2)
+
+  def test_sized_add_9_3(self):
+    self._test_sized_add(9, 3)
+
+  def test_sized_add_10_3(self):
+    self._test_sized_add(10, 3)
+
+  def test_sized_add_11_3(self):
+    self._test_sized_add(11, 3)
+
+  def test_sized_add_11_4(self):
+    self._test_sized_add(11, 4)
+
+  def _test_unsized_add(self, length, alignment):
+    data, chunks = self._generate_data(length, alignment)
+    # Test all possible permutations
+    for sequence in itertools.permutations(chunks):
+      assembler = util.DataAssembler(alignment)
+      for chunk in sequence:
+        assembler.add(chunk.data, chunk.offset)
+      self.assertEquals(data, assembler.getbytes())
+      # Same test with add_chunk()
+      assembler = util.DataAssembler(alignment)
+      for chunk in sequence:
+        assembler.add_chunk(chunk)
+      self.assertEquals(data, assembler.getbytes())
+
+  def test_unsized_add_3_1(self):
+    self._test_unsized_add(3, 1)
+
+  def test_unsized_add_9_2(self):
+    self._test_unsized_add(9, 2)
+
+  def test_unsized_add_9_3(self):
+    self._test_unsized_add(9, 3)
+
+  def test_unsized_add_10_3(self):
+    self._test_unsized_add(10, 3)
+
+  def test_unsized_add_11_3(self):
+    self._test_unsized_add(11, 3)
+
+  def test_unsized_add_11_4(self):
+    self._test_unsized_add(11, 4)
+
 
 class TestUtil(unittest.TestCase):
   def test_ipv4_to_bytes(self):
@@ -53,6 +253,7 @@ class TestUtil(unittest.TestCase):
       util.ipv4_to_chunk('1.128.300.1')
     with self.assertRaises(ValueError):
       util.ipv4_to_chunk('1.128.64.1999')
+
 
 if __name__ == '__main__':
   unittest.main()
--- vodreassembler/util.py
+++ vodreassembler/util.py
@@ -1,11 +1,176 @@
 """Miscellaneous utility functions and classes."""
 
+import bitarray
 import collections
+import io
 import struct
+import types
+
+
+class Error(Exception):
+  pass
+
+
+class IncompleteDataError(Error):
+  pass
+
+
+class UnexpectedChunkError(Error):
+  pass
+
+
+class ChunkPastEndError(UnexpectedChunkError):
+  pass
+
+
+class ChunkCollisionError(UnexpectedChunkError):
+  pass
 
 
 class DataChunk(collections.namedtuple('DataChunk', ['data', 'offset'])):
   pass
+
+
+class DataAssembler:
+  """Assembler for DataChunk objects.
+  
+  DataChunk represents a part of the entire data. DataAssembler can reconstruct
+  the entire data by assembling multiple DataChunk objects into a single buffer.
+  Each chunk is expected to be obtained by splitting the original data for every
+  alignment bytes.
+  """
+  def __init__(self, alignment, storage=None, length=None):
+    """Initialize DataAssembler for accepting chunks of given alignment.
+
+    DataAssembler object takes ownership of provided storage object. Therefore,
+    the storage should not be modified externally.
+
+    Args:
+      alignment (int): Alignment of each chunk. Offset of each chunk added must
+        be divisible by this number. Also, every chunk but the last must have
+        the same length as alignment.
+      storage (optional): binary file object used as data storage for the
+        assembler. Must support both read and write. If not provided, io.BytesIO
+        is used.
+      length (int, optional): Length of the entire data. Used to perform extra
+        validations.
+    """
+    if not isinstance(alignment, int):
+      raise TypeError("alignment "
+                      "must be int".format(self.__init__.__name__))
+    elif alignment <= 0:
+      raise ValueError('alignment must be positive integer')
+     
+    if length is not None:
+      if not isinstance(length, int):
+        raise TypeError("{}: length "
+                        "must be int or None".format(self.__init__.__name__))
+      elif length < 0:
+        raise ValueError("{}: length "
+                         "cannot be negative".format(self.__init__.__name__))
+
+    bitarray_length = 1 + (length - 1) // alignment if length is not None else 1
+    self._alignment = alignment
+    self._has_chunk = bitarray.bitarray(bitarray_length)
+    self._storage = storage or io.BytesIO(b'\x00' * (length or 0))
+    self._length = length
+
+    self._has_chunk.setall(False)
+
+  @property
+  def alignment(self):
+    return self._alignment
+
+  @property
+  def complete(self):
+    return self._has_chunk.all()
+
+  @property
+  def length(self):
+    return self._length
+
+  def getbytes(self, incomplete=False):
+    if not incomplete and not self.complete:
+      raise IncompleteDataError('cannot return incomplete data')
+
+    curr = self._storage.seek(0)
+    if curr != 0:
+      raise RuntimeError('seek(0) failed')
+    return self._storage.read()
+
+  def add(self, data, offset):
+    length = len(data)
+    self._verify_chunk_params(length, offset)
+    self._update_bitmap(length, offset)
+
+    curr = self._storage.seek(offset)
+    if curr < offset:
+      # Seek to the offset failed somehow. Try padding to fill in the gap.
+      self._storage.write(b'\x00' * (offset - curr))
+    if self._storage.tell() != offset:
+      raise RuntimeError('failed to seek to offset {}'.format(offset))
+    assert self._storage.tell() == offset
+    self._storage.write(data)
+
+  def _is_last_chunk(self, offset, length):
+    if self._length is None:
+      return offset >= self._alignment * len(self._has_chunk)
+    return offset + self._alignment >= self._length
+
+  def _verify_chunk_params(self, length, offset):
+    assert length >= 0
+
+    if offset < 0:
+      raise ValueError('offset must be non-negative')
+    elif self._length is not None and offset >= self._length:
+      raise ChunkPastEndError('chunk at offset {} '
+                              'is past the end of expected range')
+
+    if offset % self._alignment != 0:
+      raise ValueError('offset not aligned by {} bytes'.format(self._alignment))
+
+    if length > self._alignment:
+      raise ValueError('length of the chunk must not exceed the alignment')
+
+    chunk_index = offset // self._alignment
+    try:
+      if self._has_chunk[chunk_index]:
+        raise ChunkCollisionError('chunk at offset {} '
+                                  'has been already added'.format(offset))
+    except IndexError:
+      # Offset is already checked above, so the only case should be when the
+      # length is unknown.
+      assert self._length is None
+
+    if self._is_last_chunk(offset, length):
+      if self._length and offset + length != self._length:
+        # This is the last chunk, which should fill the buffer without gap or
+        # overflow.
+        raise ValueError('incorrect length {} for the last chunk'.format(length))
+    else:
+      if length != self._alignment:
+        # Not the last chunk; data length must match alignment.
+        raise ValueError('length of every chunk but the last '
+                         'must match alignment.')
+
+  def _update_bitmap(self, length, offset):
+    assert length >= 0
+    assert offset >= 0
+
+    chunk_index = offset // self._alignment
+    if chunk_index >= len(self._has_chunk):
+      assert self._length is None
+      # Extend the bitarray to ensure chunk_index is a valid index.
+      extension = chunk_index - len(self._has_chunk) + 1
+      self._has_chunk.extend(False for i in range(extension))
+
+    self._has_chunk[chunk_index] = True
+    if self._length is None and length < self._alignment:
+      # This must be the last chunk; now we know the length.
+      self._length = offset + length
+
+  def add_chunk(self, chunk):
+    self.add(*chunk)
 
 
 def ipv4_to_bytes(addr):
