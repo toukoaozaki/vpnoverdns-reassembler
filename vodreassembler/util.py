90c534d0183d0abe3d27873613859be2506093f0
Eunsoo Roh <roh7@illinois.edu>
2015-05-06 10:53:09 -0500

    Separate protocol utilities from generic utilities.

--- vodreassembler/util.py
+++ vodreassembler/util.py
@@ -3,7 +3,6 @@
 import bitarray
 import collections
 import io
-import struct
 import types
 
 
@@ -171,23 +170,3 @@ class DataAssembler:
 
   def add_chunk(self, chunk):
     self.add(*chunk)
-
-
-def ipv4_to_bytes(addr):
-  return ipv4_to_chunk(addr)[0]
-
-def ipv4_to_chunk(addr):
-  octets = addr.split('.')
-  if len(octets) != 4:
-    raise ValueError('IPv4 addresses must have 4 octets')
-  octets = list(map(int, octets))
-  # Every octet must fall in range [0,255]
-  assert len(octets) == 4
-  for i, o in enumerate(octets):
-    if not 0 <= o <= 255:
-      raise ValueError('Octet {} is {};'
-                       'must be an integer within [0,255]'.format(i+1, o))
-  length = (octets[0] >> 6) & 0x3
-  offset = (octets[0] & 0x3f) * 3
-  data = b''.join(map(lambda x: struct.pack('!B', x), octets[1:length+1]))
-  return DataChunk(data, offset)
