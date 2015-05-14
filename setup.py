8e5eb1ded624be474dbc24639b21a7e71272e037
Eunsoo Roh <roh7@illinois.edu>
2015-05-13 20:27:06 -0700

    Added basic setup.py script

---
+++ setup.py
@@ -0,0 +1,83 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='vpnoverdns-reassembler',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.dev',
+
+    description='Libraries and tools for parsing vpnoverdns communications.',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/toukoaozaki/vpnoverdns-reassembler',
+
+    # Author details
+    author='Eunsoo Roh',
+    author_email='toukoaozaki@gmail.com',
+
+    # Choose your license
+    license='Apache License 2.0',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        # Audience and topic
+        'Intended Audience :: Developers',
+        'Topic :: Internet',
+        # License
+        'License :: OSI Approved :: Apache Software License',
+        # Versions supported
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    # What does your project relate to?
+    keywords='vpnoverdns',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['bitarray', 'regex'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'vodparse=vodreassembler.cli.parser:main',
+        ],
+    },
+)
